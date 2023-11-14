4674fbf0ae7510b6a680c3f0878c7d9e82da350c
Omar <omar.oughzal@gmail.com>
2023-11-14 15:02:06 +0100

    Initial Commit

--- app/src/main/java/ma/ofppt/permissions/MainActivity.java
+++ app/src/main/java/ma/ofppt/permissions/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import ma.ofppt.permissions.databinding.ActivityMainBinding;
 
@@ -37,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
             case 1000 :
                 if(grantResults.length>0 && grantResults[0] ==PackageManager.PERMISSION_GRANTED){
                     binding.txtResultat.setText("Granted");
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Denied",Toast.LENGTH_LONG);
                 }
         }
     }
